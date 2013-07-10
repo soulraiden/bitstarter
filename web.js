7ab7dd263fcacc04be48f55d15192ce3f3b80f0b
alexio.schneeberger@gmail.com <alexio.schneeberger@gmail.com>
2013-07-10 02:25:20 +0200

    web.js modified

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
-
+var fs = require('fs'); 
+var Buffer = require('Buffer');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
